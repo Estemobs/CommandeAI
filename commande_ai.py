ff72d82d175f1a40e27215263f073e895e3dcd50
estemobsThinkpad <estemobsgaming@gmail.com>
2024-11-26 14:37:39 +0100

    adding debug to devoir cmd

--- commande_ai.py
+++ commande_ai.py
@@ -78,84 +78,97 @@ async def improve_image_quality(image_url):
 @client.command()
 async def devoir(ctx):
     try:
+        print("Commande devoir appelée")  # Pour le débogage
         await ctx.send("Veuillez envoyer une image ou un lien vers une image valide.")
+        
+        # Attendre jusqu'à ce qu'un utilisateur envoie un message
         message = await client.wait_for('message', timeout=60.0)
-
-        if not ctx.message.attachments and not ctx.message.content:
-            await ctx.send("Veuillez envoyer une image ou un lien vers une image valide.")
-            return False
-
-        # Vérifie si le message contient une pièce jointe valide
-        if ctx.message.attachments:
-            attachment = ctx.message.attachments[0]
-            if not attachment.url.lower().endswith(('.png', '.jpeg', '.jpg', '.gif')):
-                await ctx.send("Le fichier joint n'est pas une image valide.")
-                return False
-
-        # Vérifie si le message contient un lien d'image valide
-        if ctx.message.content:
-            url = ctx.message.content
-            async with aiohttp.ClientSession() as session:
-                async with session.head(url) as resp:
-                    content_type = resp.headers.get('content-type')
-                    if not content_type or not content_type.startswith('image/'):
-                        await ctx.send("Le lien fourni ne pointe pas vers une image valide.")
-                        return False
-
-        # Traite la pièce jointe ou le lien d'image valide
+        
+        print("Message reçu")  # Pour le débogage
+        
+        # Vérifie si le message contient une image valide
         if message.attachments:
             attachment = message.attachments[0]
             image_url = attachment.url
-        else:
+            print(f"Image attachée : {image_url}")  # Pour le débogage
+        elif message.content:
             image_url = message.content
-
-        # Améliore la qualité de l'image
-        improved_image_bytes = await improve_image_quality(image_url)
-
-        # Envoie l'image améliorée
-        await ctx.send(file=discord.File(BytesIO(improved_image_bytes), filename="improved_image.jpg"))
-
-        # Extrait le texte de l'image et l'affiche
-        text = extract_text_from_image(image_url)
-        await display_text(ctx, text)
+            print(f"Lien de l'image : {image_url}")  # Pour le débogage
+        else:
+            print("Aucune image ou lien trouvé")  # Pour le débogage
+            return await ctx.send("Veuillez envoyer une image ou un lien vers une image valide.")
         
+        # Améliore la qualité de l'image
+        try:
+            improved_image_bytes = await improve_image_quality(image_url)
+            print("Image améliorée")  # Pour le débogage
+        except Exception as e:
+            print(f"Erreur lors de l'amélioration de l'image : {str(e)}")
+            return await ctx.send("Une erreur s'est produite lors de l'amélioration de l'image.")
+        
+        # Envoie l'image améliorée
+        try:
+            await ctx.send(file=discord.File(BytesIO(improved_image_bytes), filename="improve_image.jpg"))
+            print("Image envoyée")  # Pour le débogage
+        except Exception as e:
+            print(f"Erreur lors de l'envoi de l'image : {str(e)}")
+            return await ctx.send("Une erreur s'est produite lors de l'envoi de l'image.")
+        
+        # Extrait le texte de l'image et l'affiche
+        try:
+            text = extract_text_from_image(image_url)
+            print(f"Texte extrait : {text}")  # Pour le débogage
+            await display_text(ctx, text)
+        except Exception as e:
+            print(f"Erreur lors de l'extraction du texte : {str(e)}")
+            return await ctx.send("Une erreur s'est produite lors de l'extraction du texte.")
         
         # Utilise OpenAI pour générer les réponses de l'exercice
-        options = webdriver.ChromeOptions()
-        #options.add_argument("--headless")  # Active le mode headless
-        options.add_argument("executable_path=chromedriver.exe")
-        driver = webdriver.Chrome(options=options)
-        driver.get("https://chatgpt.com/")
-        driver.set_window_size(1278, 974)
-        element = driver.find_element(By.ID, "prompt-textarea")
-        driver.execute_script("if(arguments[0].contentEditable === 'true') {arguments[0].innerText = '<p data-placeholder=\"Message ChatGPT\" class=\"placeholder\"><br class=\"ProseMirror-trailingBreak\"></p>'}", element)
-        driver.find_element(By.LINK_TEXT, "Stay logged out").click()
-        driver.find_element(By.CSS_SELECTOR, ".placeholder").click()
-        element = driver.find_element(By.ID, "prompt-textarea")
-        driver.save_screenshot('capture_ecran1.png')
+        try:
+            # Utilise OpenAI pour générer les réponses de l'exercice
+            options = webdriver.ChromeOptions()
+            #options.add_argument("--headless")  # Active le mode headless
+            options.add_argument("executable_path=chromedriver.exe")
+            driver = webdriver.Chrome(options=options)
+            driver.get("https://chatgpt.com/")
+            driver.set_window_size(1278, 974)
+            element = driver.find_element(By.ID, "prompt-textarea")
+            driver.execute_script("if(arguments[0].contentEditable === 'true') {arguments[0].innerText = '<p data-placeholder=\"Message ChatGPT\" class=\"placeholder\"><br class=\"ProseMirror-trailingBreak\"></p>'}", element)
+            driver.find_element(By.LINK_TEXT, "Stay logged out").click()
+            driver.find_element(By.CSS_SELECTOR, ".placeholder").click()
+            element = driver.find_element(By.ID, "prompt-textarea")
+            driver.save_screenshot('capture_ecran1.png')
+            
+            # Après avoir extrait le texte de l'image et avant d'utiliser Selenium
+            driver.execute_script(f"if(arguments[0].contentEditable === 'true') {{arguments[0].innerText = '<p>répond a l\'exercice suivant :</p><p>{text}</p>';}}", element)
+            element = driver.find_element(By.CSS_SELECTOR, ".icon-md-heavy > path")
+            driver.save_screenshot('capture_ecran2.png')
+            actions = ActionChains(driver)
+            actions.move_to_element(element).perform()
+            element = driver.find_element(By.CSS_SELECTOR, "body")
+            driver.save_screenshot('capture_ecran3.png')
+            
+            # Après avoir exécuté les actions nécessaires pour obtenir la réponse
+            response_element = driver.find_element(By.CSS_SELECTOR, ".markdown > p")
+            text_response = response_element.text
+            driver.save_screenshot('capture_ecran4.png')
         
-        # Après avoir extrait le texte de l'image et avant d'utiliser Selenium
-        driver.execute_script(f"if(arguments[0].contentEditable === 'true') {{arguments[0].innerText = '<p>répond a l\'exercice suivant :</p><p>{text}</p>';}}", element)
-        element = driver.find_element(By.CSS_SELECTOR, ".icon-md-heavy > path")
-        driver.save_screenshot('capture_ecran2.png')
-        actions = ActionChains(driver)
-        actions.move_to_element(element).perform()
-        element = driver.find_element(By.CSS_SELECTOR, "body")
-        driver.save_screenshot('capture_ecran3.png')
         
-        # Après avoir exécuté les actions nécessaires pour obtenir la réponse
-        response_element = driver.find_element(By.CSS_SELECTOR, ".markdown > p")
-        text_response = response_element.text
-        driver.save_screenshot('capture_ecran4.png')
-        
-        
-        # Envoie les réponses de l'exercice
-        await ctx.send(f"Réponse de ChatGPT : {text_response}")
+            # Envoie les réponses de l'exercice
+            await ctx.send(f"Réponse de ChatGPT : {text_response}")
 
+        except Exception as e:
+                print(f"Erreur lors de la génération des réponses : {str(e)}")
+                return await ctx.send("Une erreur s'est produite lors de la génération des réponses.")
+        
+    
     except asyncio.TimeoutError:
-        await ctx.send("Vous n'avez pas envoyé d'image dans le délai imparti.")
+        print("Timeout atteint")
+        return await ctx.send("Vous n'avez pas envoyé d'image dans le délai imparti.")
     except Exception as e:
-        await ctx.send(f"Une erreur s'est produite : {str(e)}")
+        print(f"Une erreur inattendue s'est produite : {str(e)}")
+        return await ctx.send(f"Une erreur inattendue s'est produite lors de l'exécution de la commande")
+        
 
 # Lire les secrets à partir du fichier JSON
 with open("secrets.json", "r") as file:
